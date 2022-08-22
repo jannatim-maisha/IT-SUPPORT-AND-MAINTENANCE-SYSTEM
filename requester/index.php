43e591ae1a1d612e0379b6eb8cbd4b840efe1d83
anab028 <107667434+anab028@users.noreply.github.com>
2022-08-23 02:41:34 +0600

    Nothing

--- requester/index.php
+++ requester/index.php
@@ -4,7 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+ <!-- Latest compiled and minified CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Latest compiled JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="css/all.min.css">
     <link rel="stylesheet" href="css/custom.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -17,14 +21,165 @@
     
 <!--- Start Navigation ---->
 
-<nav  class="navbar navbar-expand-sm navbar-dark bg-danger pl-5 fixed-top" >
-
-
-
+<header class="header">
+  
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">ISMS</a>
+	
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span ></span>
+	   <span ></span>
+	    <span ></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-between " id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+        <li class="nav-item ">
+          <a class="nav-link acrive" aria-current="page" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Services</a>
+        </li>
+		 <li class="nav-item">
+          <a class="nav-link" href="#">Registration</a>
+        </li>
+		 <li class="nav-item">
+          <a class="nav-link" href="#">Login</a>
+        </li>
+		 <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+      
+       
+      </ul>
+     
+    </div>
+  </div>
 </nav>
+  
+  </header>
 
 <!--- end of navigation -----> 
 
+<!----slider --------> 
+
+<section>
+
+<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/3.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>WELCOME TO ISMS SERIVCE CENTER</h5>
+        <p>Some representative placeholder content for the first slide.</p>
+		<div  class="slider-btn">
+		
+		<button class="btn btn-1">LogIn</button>
+		<button class="btn btn-2">SignUp</button>
+		
+		</div>
+		
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="images/2.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>WELCOME TO ISMS SERIVCE CENTER</h5>
+        <p>Some representative placeholder content for the second slide.</p>
+		<div  class="slider-btn">
+		
+		<button class="btn btn-1">LogIn</button>
+		<button class="btn btn-2">SignUp</button>
+		
+		</div>
+		
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="images/1.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>WELCOME TO ISMS SERIVCE CENTER</h5>
+        <p>Some representative placeholder content for the third slide.</p>
+		
+		<div  class="slider-btn">
+		
+		<button class="btn btn-1">LogIn</button>
+		<button class="btn btn-2">SignUp</button>
+		
+		</div>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+</section>
+
+
+<!----slider End here --------> 
+
+
+<!----Introduction  --------> 
+
+ 
+
+<div class="container">
+
+<div class="mt-4 p-5 bg-primary text-white rounded">
+
+<h1 class="text-center">About US</h1>
+
+<p>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+ ut labore et dolore magna aliqua.consectetur adipiscing elit  consectetur adipiscing elit
+ consectetur adipiscing elit  consectetur adipiscing elit  consectetur adipiscing elit
+ consectetur adipiscing elit consectetur adipiscing elit consectetur adipiscing elit
+
+ Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+ ut labore et dolore magna aliqua.consectetur adipiscing elit  consectetur adipiscing elit
+ consectetur adipiscing elit  consectetur adipiscing elit  consectetur adipiscing elit
+ consectetur adipiscing elit consectetur adipiscing elit consectetur adipiscing elit
+ 
+ Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+ ut labore et dolore magna aliqua.consectetur adipiscing elit  consectetur adipiscing elit
+ consectetur adipiscing elit  consectetur adipiscing elit  consectetur adipiscing elit
+ consectetur adipiscing elit consectetur adipiscing elit consectetur adipiscing elit
+
+ 
+</p>
+</div>
+
+</div>
+
+
+
+<!----Introduction End here --------> 
+
+
+
+
+
+
+
+
+
+<!-- Bootstrap JavaScript -->
+
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 
 </body>
 </html>
