94b8be665f4195c9349961be55de8ee7a94588bd
Jannatim Maisha <maishapousy@gmail.com>
2022-08-23 23:33:59 +0600

    home er kichu

--- index.php
+++ index.php
@@ -167,6 +167,24 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor i
 
 <!----Introduction End here --------> 
 
+ <!-- Start Services -->
+ <div class="container text-center border-bottom" id="Services">
+    <h2>Our Services</h2>
+    <div class="row mt-4">
+      <div class="col-sm-4 mb-4">
+        <a href="#"><i class="fas fa-tv fa-8x text-success"></i></a>
+        <h4 class="mt-4">Electronic Appliances</h4>
+      </div>
+      <div class="col-sm-4 mb-4">
+        <a href="#"><i class="fas fa-sliders-h fa-8x text-primary"></i></a>
+        <h4 class="mt-4">Preventive Maintenance</h4>
+      </div>
+      <div class="col-sm-4 mb-4">
+        <a href="#"><i class="fas fa-cogs fa-8x text-info"></i></a>
+        <h4 class="mt-4">Fault Repair</h4>
+      </div>
+    </div>
+  </div> <!-- End Services -->
 
 
 
