cdd0288cf42491468494c8d2e9631ead1f22f2f8
anab028 <107667434+anab028@users.noreply.github.com>
2022-08-27 21:52:51 +0600

    dbconnection

--- index.php
+++ index.php
@@ -50,6 +50,9 @@
 		 <li class="nav-item">
           <a class="nav-link" href="#Contact">Contact</a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#Contact">Shop Now</a>
+        </li>
       
        
       </ul>
