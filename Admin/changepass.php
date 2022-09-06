bcf820ce708fc872bdeea6db0c43211f3ee05858
Jannatim Maisha <maishapousy@gmail.com>
2022-09-06 10:12:38 +0600

    password change

--- Admin/changepass.php
+++ Admin/changepass.php
@@ -1,127 +1,57 @@
-
-
 <?php
-define('TITLE', 'Dashboard');
-define('PAGE', 'dashboard');
+define('TITLE', 'Change Password');
+define('PAGE', 'changepass');
+include('includes/header.php'); 
 include('../dbConnection.php');
+session_start();
+ if(isset($_SESSION['is_adminlogin'])){
+  $aEmail = $_SESSION['aEmail'];
+ } else {
+  echo "<script> location.href='login.php'; </script>";
+ }
+ $aEmail = $_SESSION['aEmail'];
+ if(isset($_REQUEST['passupdate'])){
+  if(($_REQUEST['aPassword'] == "")){
+   //  if required field missing
+   $passmsg = '<div class="alert alert-warning col-sm-6 ml-5 mt-2" role="alert"> Fill All Fileds </div>';
+  } else {
+    $sql = "SELECT * FROM adminlogin_tb WHERE a_email='$aEmail'";
+    $result = $conn->query($sql);
+    if($result->num_rows == 1){
+     $aPass = $_REQUEST['aPassword'];
+     $sql = "UPDATE adminlogin_tb SET a_password = '$aPass' WHERE a_email = '$aEmail'";
+      if($conn->query($sql) == TRUE){
+       
+       $passmsg = '<div class="alert alert-success col-sm-6 ml-5 mt-2" role="alert"> Updated Successfully </div>';
+      } else {
+       
+       $passmsg = '<div class="alert alert-danger col-sm-6 ml-5 mt-2" role="alert"> Unable to Update </div>';
+      }
+    }
+   }
+}
 ?>
-
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
- <meta charset="UTF-8">
- <meta name="viewport" content="width=device-width, initial-scale=1.0">
- <meta http-equiv="X-UA-Compatible" content="ie=edge">
- <title>
-  dashboard
- </title>
- <!-- Bootstrap CSS -->
- <link rel="stylesheet" href="../css/bootstrap.min.css">
-
- <!-- Font Awesome CSS -->
- <link rel="stylesheet" href="../css/all.min.css">
-
- <!-- Custome CSS -->
- <link rel="stylesheet" href="../css/custom.css">
-
-</head>
-
-<body>
- <!-- Top Navbar -->
- <nav class="navbar navbar-dark fixed-top bg-primary p-0 shadow">
-  <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="dashboard.php">OSMS</a>
- </nav>
-
- <!-- Side Bar -->
- <div class="container-fluid mb-5" style="margin-top:40px;">
+<div class="col-sm-9 col-md-10">
   <div class="row">
-   <nav class="col-sm-3 col-md-2 bg-light sidebar py-5 d-print-none">
-    <div class="sidebar-sticky">
-     <ul class="nav flex-column">
-      <li class="nav-item">
-       <a class="nav-link <?php if(PAGE == 'dashboard') { echo 'active'; } ?> " href="dashboard.php">
-        <i class="fas fa-tachometer-alt"></i>
-        Dashboard
-       </a>
-      </li>
-      <li class="nav-item">
-       <a class="nav-link <?php if(PAGE == 'work') { echo 'active'; } ?>" href="work.php">
-        <i class="fab fa-accessible-icon"></i>
-        Work Order
-       </a>
-      </li>
-      <li class="nav-item">
-       <a class="nav-link <?php if(PAGE == 'request') { echo 'active'; } ?>" href="request.php">
-        <i class="fas fa-align-center"></i>
-        Requests
-       </a>
-      </li>
-      <li class="nav-item">
-       <a class="nav-link <?php if(PAGE == 'assets') { echo 'active'; } ?>" href="assets.php">
-        <i class="fas fa-database"></i>
-        Assets
-       </a>
-      </li>
-      <li class="nav-item">
-       <a class="nav-link <?php if(PAGE == 'technician') { echo 'active'; } ?>" href="technician.php">
-        <i class="fab fa-teamspeak"></i>
-        Technician
-       </a>
-      </li>
-      <li class="nav-item">
-       <a class="nav-link <?php if(PAGE == 'requesters') { echo 'active'; } ?>" href="requester.php">
-        <i class="fas fa-users"></i>
-        Requester
-       </a>
-      </li>
-      <li class="nav-item">
-       <a class="nav-link <?php if(PAGE == 'sellreport') { echo 'active'; } ?>" href="soldproductreport.php">
-        <i class="fas fa-table"></i>
-        Sell Report
-       </a>
-      </li>
-      <li class="nav-item">
-       <a class="nav-link <?php if(PAGE == 'workreport') { echo 'active'; } ?>" href="workreport.php">
-        <i class="fas fa-table"></i>
-        Work Report
-       </a>
-      </li>
-      <li class="nav-item">
-       <a class="nav-link <?php if(PAGE == 'changepass') { echo 'active'; } ?>" href="changepass.php">
-        <i class="fas fa-key"></i>
-        Change Password
-       </a>
-      </li>
-      <li class="nav-item">
-       <a class="nav-link" href="../logout.php">
-        <i class="fas fa-sign-out-alt"></i>
-        Logout
-       </a>
-      </li>
-     </ul>
+    <div class="col-sm-6">
+      <form class="mt-5 mx-5">
+        <div class="form-group">
+          <label for="inputEmail">Email</label>
+          <input type="email" class="form-control" id="inputEmail" value=" <?php echo $aEmail ?>" readonly>
+        </div>
+        <div class="form-group">
+          <label for="inputnewpassword">New Password</label>
+          <input type="text" class="form-control" id="inputnewpassword" placeholder="New Password" name="aPassword">
+        </div>
+        <button type="submit" class="btn btn-success mr-4 mt-4" name="passupdate">Update</button>
+        <button type="reset" class="btn btn-secondary mt-4">Reset</button>
+        <?php if(isset($passmsg)) {echo $passmsg; } ?>
+      </form>
     </div>
-   </nav>
-
- 
-
-
-
-
-
-
-
-
-   <!-- Boostrap JavaScript -->
-<script src="../js/jquery.min.js"></script>
-<script src="../js/popper.min.js"></script>
-<script src="../js/bootstrap.min.js"></script>
-<script src="../js/all.min.js"></script>
-<script src="../js/custom.js"></script>
-</body>
-
-</html>
-</body>
-
-</html>
+  </div>
+</div>
+</div>
+</div>
+<?php
+include('includes/footer.php'); 
+?>
